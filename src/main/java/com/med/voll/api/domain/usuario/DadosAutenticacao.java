4e0d1ae0c6501424d591a19c84f8606dd5407198
Jonatas Ferreira <jonatasoliveira@cvccorp.com.br>
2023-06-11 20:48:10 -0300

    Configuracao do Spring Security - autenticacao Stateless

---
+++ src/main/java/com/med/voll/api/domain/usuario/DadosAutenticacao.java
@@ -0,0 +1,4 @@
+package com.med.voll.api.domain.usuario;
+
+public record DadosAutenticacao(String login, String senha) {
+}
--- src/main/java/com/med/voll/api/infra/exception/TratadorDeErros.java
+++ src/main/java/com/med/voll/api/infra/exception/TratadorDeErros.java
@@ -1,4 +1,4 @@
-package com.med.voll.api.infra;
+package com.med.voll.api.infra.exception;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
